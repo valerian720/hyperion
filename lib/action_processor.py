185170d30386651d3ed4484fe415f15888b58f25
Valerian <valerian7202@gmail.com>
2020-11-19 17:52:55 +0300

    blya

--- lib/action_processor.py
+++ lib/action_processor.py
@@ -1,9 +1,4 @@
 class ConsoleMacrosProcessor(object):
-    service_mode = True
-    # выставляется в положение "истина" путем определения ключевого слова
-    # при значении "истина": 
-    # - выводит дебаг информацию
-    # - позволяет запустить утилиты для ручного редактирования знаний (mem[]) (?)
     def __init__(self, *args):
         super(ConsoleMacrosProcessor, self).__init__(*args))
         
--- lib/text_processor.py
+++ lib/text_processor.py
@@ -1,4 +1,9 @@
 class WordContextProcessor(object):
+    service_mode = True
+    # выставляется в положение "истина" путем определения ключевого слова
+    # при значении "истина": 
+    # - выводит дебаг информацию
+    # - позволяет запустить утилиты для ручного редактирования знаний (mem[]) (?)
     def __init__(self, *args):
         super(WordContextProcessor, self).__init__(*args))
         
